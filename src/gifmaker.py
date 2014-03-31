4a73f82f6f302a461b6942a5ce5c12db5fc20b6d
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-03-31 19:54:02 -0300

    Argument parser

--- src/gifmaker.py
+++ src/gifmaker.py
@@ -10,12 +10,25 @@ import re
 import subprocess
 import sys
 import tempfile
+from argparse import ArgumentParser
 
 
 RE_VIDEO_RES = 'Video:.* (\d+x\d+)[, ]'
 RE_VIDEO_FPS = 'Video:.* ([\d.]+) fps'
 
 VideoData = namedtuple('VideoData', ['path', 'width', 'height', 'fps'])
+
+def _get_arg_parser():
+    parser = ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("output")
+    parser.add_argument("-s", "--start", type=int, default=None)
+    parser.add_argument("-d", "--duration", type=int, default=None)
+    parser.add_argument("-l", "--loop", action='store_true', default=False)
+    return parser
+
+def _parse_args():
+    return _get_arg_parser().parse_args()
 
 def _extract_video_data(video):
     command = ["avprobe", video]
@@ -44,14 +57,15 @@ def _make_gif(frames_dir, output, fps, start_frame=None, end_frame=None, loop=Tr
     subprocess.call(command)
 
 def main():
-    data = _extract_video_data(sys.argv[1])
+    options = _parse_args()
+    data = _extract_video_data(options.input)
     print data
     tmp_dir = tempfile.mkdtemp()
     logging.info("Temporal dir: '%s'", tmp_dir)
     try:
         _extract_frames(data.path, 0, 1, tmp_dir)
         os.system('ls %s' % tmp_dir)
-        _make_gif(tmp_dir, sys.argv[2], data.fps)
+        _make_gif(tmp_dir, options.output, data.fps)
     finally:
         os.system("rm -rf %s" % tmp_dir)
 
