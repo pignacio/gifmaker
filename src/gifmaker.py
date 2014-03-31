7fa1c5b64894be77de3419b745b03eb50a64c32f
Ignacio Rossi <rossi.ignacio@gmail.com>
2014-03-31 20:11:16 -0300

    More logging!

--- src/gifmaker.py
+++ src/gifmaker.py
@@ -62,15 +62,20 @@ def _make_gif(frames_dir, output, fps, start_frame=None, end_frame=None, loop=Tr
     subprocess.call(command)
 
 def main():
+    logging.basicConfig(level=logging.INFO)
     options = _parse_args()
+    logging.info("Extracting video data from '%s'", options.input)
     data = _extract_video_data(options.input)
-    print data
+    logging.info("Data: %s", data)
     tmp_dir = tempfile.mkdtemp()
     logging.info("Temporal dir: '%s'", tmp_dir)
     try:
+        logging.info("Extracting frames...")
         _extract_frames(data.path, tmp_dir, options.start, options.duration)
-        os.system('ls %s' % tmp_dir)
+        logging.info("Got %s frames...", len(os.listdir(tmp_dir)))
+        logging.info("Making output gif: '%s'", options.output)
         _make_gif(tmp_dir, options.output, data.fps)
+        logging.info("Done.")
     finally:
         os.system("rm -rf %s" % tmp_dir)
 
